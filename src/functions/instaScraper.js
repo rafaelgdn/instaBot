304bbeb98d2417c10e51e513a3f55c2307d53f11
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2022-08-04 21:26:17 -0300

    handling errors improvement

--- src/functions/instaScraper.js
+++ src/functions/instaScraper.js
@@ -1,69 +1,130 @@
+/* eslint-disable no-continue */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable camelcase */
 /* eslint-disable no-promise-executor-return */
 /* eslint-disable no-console */
-import Login from '../utils/login';
-
-
+import axios from "axios";
+import { S3 } from 'aws-sdk'
+import mime from 'mime';
+import { IgApiClient } from 'instagram-private-api'
+// import Login from '../utils/login';
 
 const { INSTA_USER, INSTA_PASS } = process.env;
+const bucket = "insta-scraper-posts";
+const s3 = new S3();
+const ig = new IgApiClient();
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+ig.state.generateDevice(INSTA_USER);
+
+// const memesGeralUsers = [
+//   'smsindelicado',
+//   'pqoshomensmorre',
+//   'comediavids',
+//   'memesbrr1',
+//   'debochadaa_ofical',
+//   'memes24horasoficial',
+//   'memesbrasileiroos_',
+//   'rindoalto',
+//   'memesdolusca'
+// ]
+
+const memesFreefireUsers = [
+  'freefiire_memes',
+  'freefire.memes.brasiil',
+  'freefire.memes_zuera',
+  'freefire.memesbr2',
+  'freefirememess.br'
+]
 
 export const main = async () => {
   try {
-    const ig = await Login(INSTA_USER, INSTA_PASS);
+    // const ig = await Login(INSTA_USER, INSTA_PASS);
+    await ig.account.login(INSTA_USER, INSTA_PASS);
 
     console.log('Finding User id...');
-    const userId = await ig.user.getIdByUsername('smsindelicado');
-
-    console.log('Getting User feed', { userId });
-    const userFeed = ig.feed.user(userId);
+    const users = [];
+    for (const username of memesFreefireUsers) {
+      try {
+        const user = await ig.user.getIdByUsername(username)
+        users.push(user);
+        await sleep(getRandomIntInclusive(1000, 5000));
+      } catch (error) {
+        continue;
+      }
+    }
+    console.log('Getting Users feeds');
+    const usersFeed = await Promise.all(users.map(async (user) => ig.feed.user(user)));
 
     console.log('Scrapping Pages...');
     const pages = [];
 
     for (let i = 0; i < 10; i++) {
       try {
-        const page = await userFeed.items();
-        pages.push(page);
+        const items = [];
+
+        for (const userFeed of usersFeed) {
+          try {
+            const item = await userFeed.items();
+            items.push(item);
+            await sleep(getRandomIntInclusive(1000, 5000));
+          } catch (error) {
+            continue;
+          }
+        }
+
+        pages.push(items.flat());
       } catch (error) {
-        break;
+        continue;
       }
     }
 
     const feedPages = pages.flat();
     console.log('Items length', { length: feedPages.length });
 
-    await Promise.all(feedPages.map(async (page) => {
+    for (const page of feedPages) {
       console.log('Getting media url...');
       const mediaId = page.caption?.media_id;
-      const data = mediaId && (await ig.media.info(mediaId));
-      const text = data?.items?.[0]?.caption.text;
-      const imgUrl = data?.items?.[0]?.image_versions2?.candidates?.[0]?.url;
-      const videoUrl = data?.items?.[0]?.video_versions?.[0]?.url;
+      const info = mediaId && (await ig.media.info(mediaId));
+      const text = info?.items?.[0]?.caption.text;
+      const imgUrl = info?.items?.[0]?.image_versions2?.candidates?.[0]?.url;
+      const videoUrl = info?.items?.[0]?.video_versions?.[0]?.url;
       const media = videoUrl || imgUrl;
-      console.log({ text, videoUrl, imgUrl, media });
-      console.log({ feedPages, lenght: feedPages.length });
-    }))
 
+      if (!media) continue;
 
+      const resp = await axios.get(media, { responseType: "stream" });
+      const contentType = resp.headers['content-type'];
+      const ext = mime.getExtension(contentType);
 
+      await s3.upload({
+        Bucket: bucket,
+        ACL: 'private',
+        Key: `memes/freefire/${mediaId}/${mediaId}.${ext}`,
+        Body: resp.data,
+      }).promise();
+
+      await s3.upload({
+        Bucket: bucket,
+        ACL: 'private',
+        Key: `memes/freefire/${mediaId}/text.txt`,
+        Body: text,
+      }).promise();
+
+      await sleep(getRandomIntInclusive(3000, 10000));
+    }
+
+    return 'Successfully scraped!'
   } catch (error) {
     console.log('First error', { error });
     throw error;
-  }
-
-
-
-  // const pageOne = await userFeed.items();
-  // const pageTwo = await userFeed.items();
-  // const pagethree = await userFeed.items();
-  // const pageFour = await userFeed.items();
-  // const pageFive = await userFeed.items();
-
-  // const feedPages = [...pageOne, ...pageTwo, ...pagethree, ...pageFour, ...pageFive];
-
-
-
-  return 'Successfully scraped!';
+  };
 };
